79e0e18397c79f4eb31e9f40c31c1c98c0e1b87f
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-10-17 18:23:10 +0530

    Remove auth0 authentication

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,6 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
   return (
     <nav>
       <div className="flex space-between h-20 items-center">
@@ -10,23 +8,21 @@ const Navbar = () => {
           Chat
         </NavLink>
         <div>
-          {isAuthenticated ? (
-            <button
-              className="p-3 px-5 bg-blue-600 text-white"
-              onClick={() =>
-                logout({ logoutParams: { returnTo: window.location.origin } })
-              }
-            >
-              Log Out
-            </button>
-          ) : (
-            <button
-              className="p-3 px-5 bg-blue-600 text-white"
-              onClick={() => loginWithRedirect()}
-            >
-              Log In
-            </button>
-          )}
+          <button
+            className="p-3 px-5 bg-blue-600 text-white"
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Log Out
+          </button>
+
+          <button
+            className="p-3 px-5 bg-blue-600 text-white"
+            onClick={() => loginWithRedirect()}
+          >
+            Log In
+          </button>
         </div>
       </div>
     </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import { Auth0Provider } from '@auth0/auth0-react';
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <Auth0Provider
-    domain="dev-dwtbiis1z7ashcvb.us.auth0.com"
-    clientId="yrgamTPPIq2kczCZYLne11XDSzdDRpjy"
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
+  <StrictMode>
     <App />
-  </Auth0Provider>
+  </StrictMode>
 );
