37b64440c6e0372bd5d7039da638a828904eefd9
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-10-18 08:55:15 +0530

    Fix routes error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Chat from "./pages/Chat";
 import Header from "./components/Header";
