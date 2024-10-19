a9e162dd02bf21603130ebbf6923e180cd10429f
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-10-19 22:06:19 +0530

    Create landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,37 @@
 import React from "react";
-import { TextField } from "@mui/material"; 
+import { Grid } from "@mui/material";
 
 const Home = () => {
   return (
-    <section className="container flex justify-center h-screen">
-      <h1 className="text-3xl mt-4">AI Chatbot Landing Page</h1>
+    <section className="w-9/12 mr-auto ml-auto h-screen m-10">
+      <Grid container spacing={6}>
+        <Grid item md={5} xs={12}>
+          <div>
+            <p className="text-xl mb-5 mt-10">With Tejix AI</p>
+            <p className="text-[75px] font-bold leading-[100px] mb-10">
+              Talk To The Future
+            </p>
+            <p className="text-justify">
+              Step into a world of intelligent conversations with Tejix chatbot.
+              Your virtual companion understands you, learns from you, and
+              engages in meaningful dialogues.
+            </p>
+            <a
+              href="/login"
+              className="p-6 px-16 mt-10 w-full bg-pink-500 text-white hover:bg-pink-600 rounded-xl text-xl flex items-center justify-center"
+            >
+              TRY NOW
+            </a>
+          </div>
+        </Grid>
+        <Grid item md={7} xs={12}>
+          <img
+            id="img1"
+            src="/images/landing.webp"
+            className="rounded-full w-full h-full object-cover p-4"
+          />
+        </Grid>
+      </Grid>
     </section>
   );
 };
