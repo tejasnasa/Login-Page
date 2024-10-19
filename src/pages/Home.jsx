12b199a003bb9ce0740baa699901604fd06290af
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-10-19 14:12:24 +0530

    Fix bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { TextField } from "@mui/material";
 const Home = () => {
   return (
     <section className="container flex justify-center">
-      <h1 className="text-3xl mt-4">AI Chatbot Home</h1>
+      <h1 className="text-3xl mt-4">AI Chatbot Landing Page</h1>
     </section>
   );
 };
